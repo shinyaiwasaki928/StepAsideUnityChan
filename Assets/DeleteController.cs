e92760e21b565b1b9cecfa26a21932af2b2404d7
shinyaiwasaki928 <79783991+shinyaiwasaki928@users.noreply.github.com>
2021-04-10 17:56:53 +0900

    追加
    DeleteCubeの回転を固定
    DeleteControllerのスクリプトの追加

--- Assets/DeleteController.cs
+++ Assets/DeleteController.cs
@@ -6,21 +6,35 @@ public class DeleteController : MonoBehaviour
 {
     //Unityちゃんのオブジェクト
     private GameObject unitychan;
+
     //UnityちゃんとDeleteCubeとの距離
     private float difference;
+
+    //DeleteCubeを移動させるコンポーネントを入れる
+    private Rigidbody myRigidbody;
+
+    //前方向の速度
+    private float velocityZ = 16f;
 
     // Start is called before the first frame update
     void Start()
     {
           //Unityちゃんのオブジェクトを取得
           this.unitychan = GameObject.Find ("unitychan");
+
           //UnityちゃんとDeleteCubeの位置(z座標)の差を求める
-          this.difference = unitychan.transform.position.z - this.transform.position.z;   
+          this.difference = unitychan.transform.position.z - this.transform.position.z;
+
+          //Rigidbodyコンポーネントを取得
+          this.myRigidbody = GetComponent<Rigidbody>();  
     }
 
     // Update is called once per frame
     void Update()
     {
+          //DeleteCubeに速度を与える
+          this.myRigidbody.velocity = new Vector3(0, 0, this.velocityZ);
+
           //Unityちゃんの位置に合わせてDeleteCubeの位置を移動
           this.transform.position = new Vector3 (0, this.transform.position.y, this.unitychan.transform.position.z-difference);   
     }

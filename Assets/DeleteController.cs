fb530e9e585d22cdc9626aad42d795dfee2c2428
shinyaiwasaki928 <79783991+shinyaiwasaki928@users.noreply.github.com>
2021-04-10 14:08:54 +0900

    Initial commit

---
+++ Assets/DeleteController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteController : MonoBehaviour
+{
+    //Unityちゃんのオブジェクト
+    private GameObject unitychan;
+    //UnityちゃんとDeleteCubeとの距離
+    private float difference;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+          //Unityちゃんのオブジェクトを取得
+          this.unitychan = GameObject.Find ("unitychan");
+          //UnityちゃんとDeleteCubeの位置(z座標)の差を求める
+          this.difference = unitychan.transform.position.z - this.transform.position.z;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          //Unityちゃんの位置に合わせてDeleteCubeの位置を移動
+          this.transform.position = new Vector3 (0, this.transform.position.y, this.unitychan.transform.position.z-difference);   
+    }
+
+    //トリガーモードで他のオブジェクトと接触した場合の処理
+    void OnTriggerEnter(Collider other)
+    {
+          //オブジェクトに衝突した場合
+          if (other.gameObject.tag == "CarTag" || other.gameObject.tag == "TrafficConeTag" || other.gameObject.tag == "CoinTag")
+          {
+                //接触したオブジェクトを破棄
+                Destroy (other.gameObject);
+          }
+    }
+}
